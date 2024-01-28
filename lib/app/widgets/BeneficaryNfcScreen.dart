72107c3bce4b9dc500e72d28cffbf54542d34129
mostafa 3tta <mostafatop985@gmail.com>
2024-01-28 12:23:40 +0200

    Optimize Design

--- lib/app/widgets/BeneficaryNfcScreen.dart
+++ lib/app/widgets/BeneficaryNfcScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smartcard/app/utils/color_manager.dart';
 import '../cubits/invoice/invoice_beneficary_cubit.dart';
 import '../models/BeneficaryNfcModel.dart';
 
@@ -14,57 +15,76 @@ class BeneficaryNfcScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<InvoiceBeneficaryCubit>(
       create: (context) => InvoiceBeneficaryCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('User Information'),
-          centerTitle: true,
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('assets/images/img_constraction.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(17),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildInfoItem('Name',
-                        '${beneficaryNfcModel.data!.firstName} ${beneficaryNfcModel.data!.lastName}'),
-                    _buildInfoItem(
-                        'Mobile', beneficaryNfcModel.data!.mobile ?? ''),
-                    _buildInfoItem(
-                        'Balance', beneficaryNfcModel.data!.balance.toString()),
-                    _buildInfoItem(
-                        'Card ID', beneficaryNfcModel.data!.cardID ?? ''),
-                    _buildInfoItem(
-                        'Address', beneficaryNfcModel.data!.address ?? ''),
-                  ],
-                ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: ColorManager.baseYellow,
+
+            title: const Text('User Information',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              const Spacer(),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
+            ),
+            centerTitle: true,
+
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(17),
                   ),
-                  onPressed: () {
-                    cubit.getPaidBeneficary(
-                        beneficaryId: beneficaryNfcModel.data!.id ?? 5,
-                        context: context);
-                  },
-//beneficaryNfcModel.data!.id ??
-                  child:
-                      const Text('Pay', style: TextStyle(color: Colors.white)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildInfoItem('Name',
+                          '${beneficaryNfcModel.data!.firstName} ${beneficaryNfcModel.data!.lastName}'),
+                      _buildInfoItem(
+                          'Mobile', beneficaryNfcModel.data!.mobile ?? ''),
+                      _buildInfoItem(
+                          'Balance', beneficaryNfcModel.data!.balance.toString()),
+                      _buildInfoItem(
+                          'Card ID', beneficaryNfcModel.data!.cardID ?? ''),
+                      _buildInfoItem(
+                          'Address', beneficaryNfcModel.data!.address ?? ''),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                const Spacer(),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+                      backgroundColor: MaterialStateProperty.all(ColorManager.secondary),
+                    ),
+                    onPressed: () {
+                      cubit.getPaidBeneficary(
+                          beneficaryId: beneficaryNfcModel.data!.id ?? 5,
+                          context: context);
+                    },
+        //beneficaryNfcModel.data!.id ??
+                    child:
+                        const Text('Pay', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/app/widgets/PaidBeneficaryScreen.dart
+++ lib/app/widgets/PaidBeneficaryScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smartcard/app/utils/color_manager.dart';
 
 import '../models/benficary_data_model.dart';
 
@@ -9,89 +10,112 @@ class PaidBeneficaryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 10,
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        image: DecorationImage(
+          image: AssetImage('assets/images/img_constraction.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: ColorManager.baseYellow,
+
+          title: const Text('الدفعات',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 0.8,
+          ),
+          centerTitle: true,
+
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 0.8,
+                  ),
+                  color: Colors.white,
                 ),
-                color: Colors.white,
+                padding: EdgeInsets.all(11),
+                margin: EdgeInsets.all(11),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                        'fullName: ${paidBeneficaryModel.beneficary!.fullName ?? ''}'),
+                    Text(
+                        'mobile: ${paidBeneficaryModel.beneficary!.mobile ?? ''}'),
+                    Text(
+                        'balance: ${paidBeneficaryModel.beneficary!.balance ?? ''}'),
+                    Text('city: ${paidBeneficaryModel.beneficary!.city ?? ''}'),
+                  ],
+                ),
               ),
-              padding: EdgeInsets.all(11),
-              margin: EdgeInsets.all(11),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                      'fullName: ${paidBeneficaryModel.beneficary!.fullName ?? ''}'),
-                  Text(
-                      'mobile: ${paidBeneficaryModel.beneficary!.mobile ?? ''}'),
-                  Text(
-                      'balance: ${paidBeneficaryModel.beneficary!.balance ?? ''}'),
-                  Text('city: ${paidBeneficaryModel.beneficary!.city ?? ''}'),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 2,
-              child: ListView.builder(
-                itemCount: paidBeneficaryModel.paidBeneficary!.date?.length ??
-                    0, // تحديد عدد العناصر
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 0.8,
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
+                child: ListView.builder(
+                  itemCount: paidBeneficaryModel.paidBeneficary!.date?.length ??
+                      0, // تحديد عدد العناصر
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 0.8,
+                        ),
+                        color: Colors.white,
                       ),
-                      color: Colors.white,
-                    ),
-                    padding: EdgeInsets.all(11),
-                    margin: EdgeInsets.all(11),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                            'cashOrCategory: ${paidBeneficaryModel.paidBeneficary!.date![index].cashOrCategory ?? ''}'),
-                        Text(
-                            'paidMoney: ${paidBeneficaryModel.paidBeneficary!.date![index].paidMoney ?? ''}'),
-                        Text(
-                            'paidDone: ${paidBeneficaryModel.paidBeneficary!.date![index].paidDone ?? ''}'),
-                        Text(
-                            'date: ${paidBeneficaryModel.paidBeneficary!.date![index].date ?? ''}'),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                      padding: EdgeInsets.all(11),
+                      margin: EdgeInsets.all(11),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                              'cashOrCategory: ${paidBeneficaryModel.paidBeneficary!.date![index].cashOrCategory ?? ''}'),
+                          Text(
+                              'paidMoney: ${paidBeneficaryModel.paidBeneficary!.date![index].paidMoney ?? ''}'),
+                          Text(
+                              'paidDone: ${paidBeneficaryModel.paidBeneficary!.date![index].paidDone ?? ''}'),
+                          Text(
+                              'date: ${paidBeneficaryModel.paidBeneficary!.date![index].date ?? ''}'),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-                onPressed: () {
-                  // _onPayButtonPressed(context, 5);
-                },
-                child: const Text('Continue',
-                    style: TextStyle(color: Colors.white)),
               ),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.all(8),
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+                    backgroundColor: MaterialStateProperty.all(ColorManager.secondary),
+                  ),
+                  onPressed: () {
+                    // _onPayButtonPressed(context, 5);
+                  },
+                  child: const Text('Continue',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
